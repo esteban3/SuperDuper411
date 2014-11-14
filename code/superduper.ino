7a8f1d5f5552b4f5a31b1207ffb799f4242feb46
scooterboo <lewasite@yahoo.com>
2014-11-13 17:57:21 -0800

    code takes sensor input from a random pin now.
    TODO: MAKE SURE THAT THE PIN GETS CHANGED TO THE MOTION DETECTOR PIN.

--- code/superduper.ino
+++ code/superduper.ino
@@ -9,40 +9,46 @@
 
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
+Servo myservo;
 Servo myservo2;
 
-int pos = 0;    // variable to store the servo position 
+const int sensorpin=5;
+
+int pos = 0;   
 int pos2 = 0;
+int i = 0;
  
 void setup() 
 { 
-  myservo.attach(10);  // attaches the servo on pin 9 to the servo object 
-  myservo2.attach(11);  // attaches the servo on pin 9 to the servo object 
-
+  myservo.attach(10);  // attaches the servo on pin 10 to the servo object 
+  myservo2.attach(11);  // attaches the servo on pin 11 to the servo object 
+  pinMode(sensorpin, INPUT);
 } 
  
 void loop() 
 { 
-  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(1);                       // waits 15ms for the servo to reach the position 
+  if(digitalRead(sensorpin)){
+    for(i=0;i<4;i++){
+      for(pos = 0; pos <= 180; pos += 1)
+      {
+        myservo.write(pos);               
+        delay(1);
+      } 
+      for(pos2 = 0; pos2 <= 180; pos2 += 1) // goes from 0 degrees to 180 degrees 
+      {                                  // in steps of 1 degree 
+        myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
+        delay(1);                       // waits 15ms for the servo to reach the position 
+      } 
+      for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+      {                                
+        myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+        delay(1);                       // waits 15ms for the servo to reach the position 
+      } 
+      for(pos2 = 180; pos2>=0; pos2-=1)     // goes from 180 degrees to 0 degrees 
+      {                                
+        myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
+        delay(1);                       // waits 15ms for the servo to reach the position 
+      }
+    } 
   } 
-  for(pos2 = 0; pos2 <= 180; pos2 += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
-    delay(1);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(1);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos2 = 180; pos2>=0; pos2-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
-    delay(1);                       // waits 15ms for the servo to reach the position 
-  } 
-} 
+}
