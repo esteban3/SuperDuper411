5459a74b0d2c97773e6648783c996c24cd6a4d0b
scooterboo <lewasite@yahoo.com>
2014-11-18 13:04:07 -0800

    Update to the code
    All is working on our current prototype, but we need to test the code on hardware that has the correct clock frequency.

--- code/superduper.ino
+++ code/superduper.ino
@@ -1,10 +1,8 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com> 
- This example code is in the public domain.
+/* 
+Super Duper servo controlling program.
 
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://arduino.cc/en/Tutorial/Sweep
+when a voltage is sensed on the motion sensor pin,
+twirl the servos and blink the lasor 5 times.
 */ 
 
 #include <Servo.h> 
@@ -12,7 +10,8 @@
 Servo myservo;
 Servo myservo2;
 
-const int sensorpin=5;
+const int sensorpin=A0; //A0 is the sensor pin
+const int laserpin =8;  //D8 is the lasor pin
 
 int pos = 0;   
 int pos2 = 0;
@@ -20,35 +19,41 @@ int i = 0;
  
 void setup() 
 { 
-  myservo.attach(10);  // attaches the servo on pin 10 to the servo object 
-  myservo2.attach(11);  // attaches the servo on pin 11 to the servo object 
-  pinMode(sensorpin, INPUT);
+  myservo.attach(9);  // attaches the servo on pin 10 to the servo object 
+  myservo2.attach(10);  // attaches the servo on pin 11 to the servo object 
+  pinMode(laserpin, OUTPUT);
 } 
  
 void loop() 
 { 
-  if(digitalRead(sensorpin)){
+  if(analogRead(sensorpin)>=400){
     for(i=0;i<4;i++){
+        digitalWrite(laserpin, HIGH);  
+      //move both servos back and forth so fast that it blends into a circle.
       for(pos = 0; pos <= 180; pos += 1)
       {
         myservo.write(pos);               
         delay(1);
       } 
-      for(pos2 = 0; pos2 <= 180; pos2 += 1) // goes from 0 degrees to 180 degrees 
-      {                                  // in steps of 1 degree 
-        myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
-        delay(1);                       // waits 15ms for the servo to reach the position 
-      } 
-      for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-      {                                
-        myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-        delay(1);                       // waits 15ms for the servo to reach the position 
-      } 
-      for(pos2 = 180; pos2>=0; pos2-=1)     // goes from 180 degrees to 0 degrees 
-      {                                
-        myservo2.write(pos2);              // tell servo to go to position in variable 'pos' 
-        delay(1);                       // waits 15ms for the servo to reach the position 
+      for(pos2 = 0; pos2 <= 180; pos2 += 1)  
+      {                                   
+        myservo2.write(pos2);               
+        delay(1);                        
       }
+          digitalWrite(laserpin, LOW);
+      for(pos = 180; pos>=0; pos-=1)      
+      {                                
+        myservo.write(pos);               
+        delay(1);                        
+      } 
+      for(pos2 = 180; pos2>=0; pos2-=1)     
+      {                                
+        myservo2.write(pos2);             
+        delay(1);                        
+      }
+      
+      
+      
     } 
   } 
 }
